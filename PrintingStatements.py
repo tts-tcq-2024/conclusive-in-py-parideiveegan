19194ebd7c9a1605c6257d2ca30d0761253c005c
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-28 17:54:47 +0530

    Update PrintingStatements.py

--- PrintingStatements.py
+++ PrintingStatements.py
@@ -1,11 +1,11 @@
-Mock_breachtype = ''
+Mock_breachType = ''
 Mock_header = ''
 Mock_recepient = ''
 class PrintingStatement:
-  def printStatement_to_controller(self, header,breachtype):
+  def printStatement_to_controller(self, header,breachType):
     header = 0xfeed
     print(f'{header}, {breachType}')
-  def printStatement_to_email(self, recepient, breachtype):
+  def printStatement_to_email(self, recepient, breachType):
     recepient = "a.b@c.com"
     if breachType == 'TOO_LOW':
       print(f'To: {recepient}')
@@ -13,9 +13,9 @@ class PrintingStatement:
     elif breachType == 'TOO_HIGH':
       print(f'To: {recepient}')
       print('Hi, the temperature is too high')
-  def MockprintStatement_to_controller(self, header,breachtype):
-    Mock_brechtype = breachtype
+  def MockprintStatement_to_controller(self, header,breachType):
+    Mock_brechType = breachtype
     Mock_header = header
-  def MockprintStatement_to_email(self, recepient,breachtype):
-    Mock_brechtype = breachtype
+  def MockprintStatement_to_email(self, recepient,breachType):
+    Mock_brechType = breachType
     Mock_recepient = recepient
