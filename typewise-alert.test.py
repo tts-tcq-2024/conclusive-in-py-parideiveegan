3ce00d0a60989fcff8a78be7b5fab90b9d130481
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-28 18:42:13 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -6,8 +6,8 @@ class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(infer_breach(20, 50, 100) == 'TOO_LOW')
   def test_controller_msg(self):
-    self.assertTrue(check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL')
+    self.assertTrue(check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL' and Mock_breachType == 'NORMAL')
   def test_email_msg(self):
-    self.assertTrue(check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL')
+    self.assertTrue(check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL' and Mock_breachType == 'NORMAL')
 if __name__ == '__main__':
   unittest.main()
