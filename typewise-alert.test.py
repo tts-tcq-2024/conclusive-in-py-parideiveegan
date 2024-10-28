380628be3e103e05df184595f65f9c06c9df6b48
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-28 15:44:17 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -1,11 +1,15 @@
 import unittest
 import typewise_alert
 
-
+batteryChar = {'coolingType':'PASSIVE_COOLING'}
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-
-
+  def test_controller_msg(self):
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL' &&\
+                    breachType_Mock == 'NORMAL')
+  def test_email_msg(self):
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL' &&\
+                    breachType_Mock == 'NORMAL')
 if __name__ == '__main__':
   unittest.main()
