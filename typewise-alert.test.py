1046a90dc0ce5262d7399da97f455e7b001b4b7c
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-29 15:23:06 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -5,14 +5,19 @@ batteryChar = {'coolingType':'PASSIVE_COOLING'}
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(infer_breach(20, 50, 100) == 'TOO_LOW')
-  def test_controller_msg(self):
-    print(check_and_alert('TO_CONTROLLER',batteryChar,34))
-    print(Mock_breachType)
+  def test_controller_msg(self):  
+    
     self.assertTrue(check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL' )
-    self.assertTrue(Mock_breachType == 'NORMAL')
+    # Check if the mocked values were set correctly
+    self.assertEqual(call_print.Mock_breachType, 'NORMAL')
+    self.assertEqual(call_print.Mock_header, 0xfeed)
+    
   def test_email_msg(self):
     self.assertTrue(check_and_alert('TO_EMAIL',batteryChar,34)=='NORMAL')
-    self.assertTrue (Mock_breachType == 'NORMAL')
+    # Check if the mocked values were set correctly
+    self.assertEqual(call_print.Mock_breachType, 'NORMAL')
+    self.assertEqual(call_print.Mock_recepient, "a.b@c.com")
+    
 if __name__ == '__main__':
   print(Mock_breachType,"test")
   unittest.main()
