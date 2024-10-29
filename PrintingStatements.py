2613ffb802cfa606b452f4b7357c2a8334e1fc83
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-29 14:55:54 +0530

    Update PrintingStatements.py

--- PrintingStatements.py
+++ PrintingStatements.py
@@ -15,10 +15,8 @@ class PrintingStatement():
       print('Hi, the temperature is too high')
   def MockprintStatement_to_controller(self, header,breachType):
     Mock_breachType = breachType
-    Mock_header = header
-    print(f'{header}, {Mock_breachType}')
-  def MockprintStatement_to_email(self, recepient,breachType):
-    
+    Mock_header = header   
+  def MockprintStatement_to_email(self, recepient,breachType):    
     Mock_breachType = breachType
     Mock_recepient = recepient
-    print(Mock_breachType)
+ 
