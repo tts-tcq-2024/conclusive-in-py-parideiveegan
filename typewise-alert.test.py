5f046dfe283e337edc12564559d7e44b45d2e9f6
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-29 14:39:24 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -8,8 +8,10 @@ class TypewiseTest(unittest.TestCase):
   def test_controller_msg(self):
     print(check_and_alert('TO_CONTROLLER',batteryChar,34))
     print(Mock_breachType)
-    self.assertTrue(check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL' and Mock_breachType == 'NORMAL')
+    self.assertTrue(check_and_alert('TO_CONTROLLER',batteryChar,34)=='NORMAL' )
+    self.assertTrue(Mock_breachType == 'NORMAL')
   def test_email_msg(self):
     self.assertTrue(check_and_alert('TO_EMAIL',batteryChar,34)=='NORMAL' and Mock_breachType == 'NORMAL')
+    self.assertTrue (Mock_breachType == 'NORMAL')
 if __name__ == '__main__':
   unittest.main()
